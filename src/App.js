51cf7398263c748ddc6adde97a4f596bd36ca4c1
Lucas <lffsantos@gmail.com>
2020-01-24 22:56:26 -0300

    learning react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header.js'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+import Generos from './Generos';
+import Home from './Home';
+import NovoGenero from './NovoGenero';
+import EditarGenero from './EditarGenero.js';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Header />
+        <Route path='/' exact component={Home}></Route>
+        <Route path='/generos/:id' exact component={EditarGenero}></Route>
+        <Route path='/generos/novo' exact component={NovoGenero}></Route>
+        <Route path='/generos' exact component={Generos}></Route>
+        
+      </div>
+    </Router>
+    
+  )
 }
 
 export default App;
